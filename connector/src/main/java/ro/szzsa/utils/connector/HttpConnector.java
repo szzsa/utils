edd2a428977dfcc3001968e154416746a112b030
Attila Szasz <attila.szasz@iquestgroup.com>
2016-09-07 11:35:45 +0300

    Update http connector

--- connector/src/main/java/ro/szzsa/utils/connector/HttpConnector.java
+++ connector/src/main/java/ro/szzsa/utils/connector/HttpConnector.java
@@ -1,20 +1,37 @@
 package ro.szzsa.utils.connector;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 
 import ro.szzsa.utils.connector.exception.ConnectorException;
 import ro.szzsa.utils.connector.log.Logger;
@@ -22,26 +39,36 @@ import ro.szzsa.utils.connector.log.Loggers;
 
 public class HttpConnector implements Connector {
 
-  private static final int SOCKET_TIMEOUT = 5000;
+  private int socketTimeout = 5000;
+
+  private int connectionTimeout = 3000;
 
   private int numberOfRetries = 4;
 
   private Logger logger = Loggers.createEmptyLogger();
 
+  private String username;
+
+  private String password;
+
   @Override
   public String sendRequest(Request request) throws ConnectorException {
     int currentRetries = 0;
     Throwable exception = null;
-    CloseableHttpClient httpclient = HttpClients.createDefault();
     logger.debug("|---> Sending request to " + request.getUrl() + "\n" +
-      (request.getMessage() == null ? "" : request.getMessage()));
-    int connectionTimeout = 2000;
+                 (request.getMessage() == null ? "" : request.getMessage()));
     while (currentRetries <= numberOfRetries) {
-      try (CloseableHttpResponse httpResponse = httpclient.execute(buildHttpPost(request, connectionTimeout))) {
-        String response = getResponseMessage(httpResponse.getEntity());
-        logger.debug("|<--- Received response from " + request.getUrl() + "\n" + response);
-        return response;
-      } catch (IOException e) {
+      try (CloseableHttpClient httpclient = buildHttpClient();
+           CloseableHttpResponse httpResponse = httpclient.execute(buildHttpPost(request, connectionTimeout))) {
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        if (HttpStatus.SC_OK == statusCode) {
+          String response = getResponseMessage(httpResponse.getEntity());
+          logger.debug("|<--- Received response from " + request.getUrl() + "\n" + response);
+          return response;
+        } else {
+          throw new ConnectorException("Status code: " + statusCode);
+        }
+      } catch (KeyStoreException | NoSuchAlgorithmException | KeyManagementException | IOException e) {
         currentRetries++;
         logger.warn("Cannot connect to " + request.getUrl());
         exception = e;
@@ -51,6 +78,23 @@ public class HttpConnector implements Connector {
       }
     }
     throw new ConnectorException("Cannot connect to " + request.getUrl(), exception);
+  }
+
+  private CloseableHttpClient buildHttpClient()
+      throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    Header contentTypeHeader = new BasicHeader("content-type", "application/json; charset=utf-8");
+    SSLContext sslContext = new SSLContextBuilder()
+        .loadTrustMaterial((TrustStrategy) (chain, authType) -> true)
+        .build();
+    HostnameVerifier hostnameVerifier = (s, sslSession) -> true;
+    SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+    CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+    return HttpClients.custom()
+        .setDefaultCredentialsProvider(credentialsProvider)
+        .setDefaultHeaders(Collections.singleton(contentTypeHeader))
+        .setSSLSocketFactory(socketFactory)
+        .build();
   }
 
   private String getResponseMessage(HttpEntity entity) throws ConnectorException {
@@ -69,15 +113,22 @@ public class HttpConnector implements Connector {
   private HttpPost buildHttpPost(Request request, int connectionTimeout) throws UnsupportedEncodingException {
     HttpPost httpPost = new HttpPost(request.getUrl());
     RequestConfig config = RequestConfig.custom()
-      .setSocketTimeout(SOCKET_TIMEOUT)
-      .setConnectTimeout(connectionTimeout)
-      .build();
+        .setSocketTimeout(socketTimeout)
+        .setConnectTimeout(connectionTimeout)
+        .build();
     httpPost.setConfig(config);
     if (request.getMessage() != null) {
-      httpPost.addHeader("content-type", "application/json; charset=utf-8");
       httpPost.setEntity(new StringEntity(request.getMessage()));
     }
     return httpPost;
+  }
+
+  public void setSocketTimeout(int socketTimeout) {
+    this.socketTimeout = socketTimeout;
+  }
+
+  public void setConnectionTimeout(int connectionTimeout) {
+    this.connectionTimeout = connectionTimeout;
   }
 
   public void setNumberOfRetries(int numberOfRetries) {
@@ -86,5 +137,13 @@ public class HttpConnector implements Connector {
 
   public void setLogger(Logger logger) {
     this.logger = logger;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 }
